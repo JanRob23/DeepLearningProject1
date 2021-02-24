ec695a2831e6f3e5607a4172f1a1ab2f0045ead4
CharlieAprog <charlie.albietz.uni@gmail.com>
2021-02-24 10:24:43 +0100

    flooking at epoch nums

--- launch.py
+++ launch.py
@@ -9,34 +9,42 @@ from networks import LeNet5
 from networks import CustomNet
 from networks import linear_comb
 from plots_and_stuff import plotTrainTestPerformance
+from sklearn.model_selection import KFold
+
+
+
+
+
 
 def go(train, test):
     print('i am running')
     print('---')
-    x_train, y_train = openMNIST(train)
-    x_test, y_test = openMNIST(test)
-
-    # reshape and flip data to have it in matrix format
-    x_train = x_train.reshape(-1, 28, 28, order='C')
-    x_train = np.flip(x_train[:], 1)
-    x_test = x_test.reshape(-1, 28, 28, order='C')
-    x_test = np.flip(x_test[:], 1)
+    x_train, y_train, x_test, y_test = reshape_data(train, test)
     # confirmation plots
-    # print(y_test[600])
-    # fig = pcolor(x_test[600], cmap='gist_gray')
-    # plt.show()
+    #confirmation_plots(x_test, y_test)
+
+    #----------- LeNet 5 --------------#
+
     # print('LeNet5')
     # lenet = CustomNet()
     # model, _, _, _ = train_cnn(lenet,x_train, y_train, x_test, y_test, track_train_test_acc=True)
     # acc = eval_cnn(model,x_test, y_test)
     # print(acc)
+
+
+    #----------- CustomNet --------------#
     print('CustomNet')
     custom = CustomNet()
-    train_acc, test_acc, m_list, change = crossvalidationCNN(custom, x_train, y_train, 5)
-    plotTrainTestPerformance(train_acc, test_acc, change, m_list)
-    # model, _, _ , _= train_cnn(custom, x_train, y_train, x_test, y_test, batch_size=200)
-    # acc = eval_cnn(model,x_test, y_test)
+    #train_acc, test_acc, m_list, change = crossvalidationCNN(custom, x_train, y_train, 5)
+    
+    model, train_acc, test_acc = train_cnn(custom, x_train, y_train, x_test, y_test, batch_size=200, epochs= 200, l2_weight_decay=0.005)
+    plotTrainTestPerformance(train_acc, test_acc, 'epochs')
+    #acc = eval_cnn(model,x_test, y_test)
     #print(acc)
+
+
+    #----------- Linear Ensemble --------------#
+
     # print('linear ensample')
     # linear_co = linear_comb()
     # # # have to adjust this you dont wanna track performance over epochs, just set it to False
@@ -53,3 +61,18 @@ if __name__ == "__main__":
     test = 'data/mnist_test.csv'
     go(train,test)
 
+def reshape_data(train, test):
+    x_train, y_train = openMNIST(train)
+    x_test, y_test = openMNIST(test)
+
+    # reshape and flip data to have it in matrix format
+    x_train = x_train.reshape(-1, 28, 28, order='C')
+    x_train = np.flip(x_train[:], 1)
+    x_test = x_test.reshape(-1, 28, 28, order='C')
+    x_test = np.flip(x_test[:], 1)
+    return x_train, y_train, x_test, y_test
+
+def confirmation_plots(x_test, y_test):
+    print(y_test[600])
+    fig = pcolor(x_test[600], cmap='gist_gray')
+    plt.show()
